e54ab95025eaedc8d87238632040f7a4d65c8965
gva33 <galcantara33@gmail.com>
2019-07-21 20:39:30 -0700

    step three - not complete

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,46 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const articles = document.querySelector('.cards-container')
+const promise = axios.get('https://lambda-times-backend.herokuapp.com/articles')
+
+promise
+.then (data => {
+   console.log('RESPONSE',data.data.articles)
+   const entries = data.data.articles
+   Object.keys(entries).forEach(function (y) {
+    articles.appendChild(newComponent(entries[y]))
+    console.log(entries[y])
+   })
+})
+.catch(error => {
+    console.log('Error')
+})
+
+function newComponent(headliner,authorA,authorPhoto){
+    const card = document.createElement('div');
+    const headline = document.createElement('div');
+    const author = document.createElement('div');
+    const img = document.createElement('img');
+    const container = document.createElement('div');
+    const authorName = document.createElement('span');
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    container.classList.add('img-container');
+    
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(container);
+    author.appendChild(authorName);
+    container.appendChild(img);
+
+    headline.textContent = headliner;
+    author.textContent = authorA;
+    img.src = authorPhoto;
+
+    return card;
+
+}
